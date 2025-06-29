fa22e5eaef5b93a4b9b057af16e9d8a45e005fd2
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2025-06-29 16:49:25 +0530

    Added sonarqube required values

--- homelab-setup/nginx-proxy.sh
+++ homelab-setup/nginx-proxy.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 home_dir="homelab-setup"
+pihole_domain="pihole.nishantlabs.cloud"
+email="nishantsingh.work.503@gmail.com"
 
 # Pihole 
 sudo mkdir -p /var/lib/nginx >/dev/null
-sudo cp ${home_dir}/files/nginx/compose.yaml /var/lib/nginx/compose.yaml || exit 0
-sudo chown root:root /var/lib/nginx/compose.yaml
+sudo cp -r ${home_dir}/files/nginx/* /var/lib/nginx/. || exit 0
+sudo chown -R root:root /var/lib/nginx
 
-sudo cp ${home_dir}/files/nginx/nginx.conf /var/lib/nginx/nginx.conf
-sudo chown root:root /var/lib/nginx/nginx.conf
+sudo cp ${home_dir}/files/nginx/nginx-proxy.service /etc/systemd/system/nginx-proxy.service
+sudo chown root:root /etc/systemd/system/nginx-proxy.service
 
-sudo cp ${home_dir}/files/nginx/nginx.service /etc/systemd/system/nginx.service
-sudo chown root:root /etc/systemd/system/nginx.service
+
+sudo docker run --rm \
+  --network host \
+  -v "$(pwd)/certbot/www:/var/www/certbot" \
+  -v "$(pwd)/certbot/conf:/etc/letsencrypt" \
+  certbot/certbot certonly \
+  --webroot -w /var/www/certbot \
+  -d ${pihole_domain} \
+  --email ${email} --agree-tos --no-eff-email
+
 
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
-sudo systemctl enable --now nginx.service
-sudo systemctl restart nginx.service
+sudo systemctl enable --now nginx-proxy.service
+sudo systemctl restart nginx-proxy.service
