2b138a62590ce380463aea2a370a6f82ac00ceae
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2025-09-15 15:42:39 +0530

    update

--- homelab-setup/startup.sh
+++ homelab-setup/startup.sh
@@ -1,36 +1,8 @@
 #!/bin/bash
 
-# Prompt for SSH server name
-read -p "Enter SSH server name (default: homelab): " SERVER
-read -p "Install K8S Cluster using kubeadm (y/n): " K8S
-read -p "Install Pihole (y/n): " PIHOLE 
-read -p "Install Nginx Proxy (y/n): " NGINX
-
-# Check if input is empty
-if [[ -z "$SERVER" ]]; then
-    SERVER="homelab"
-fi
-
-
-# Figlet and Nvim
-ssh "$SERVER" "if ! which figlet >/dev/null 2>&1; then sudo apt install figlet -y >/dev/null ;fi"
-ssh "$SERVER" "if ! which nvim >/dev/null 2>&1; then sudo snap install nvim --classic >/dev/null ;fi"
-
-
-install_tools() {
-  local tool=$1
-
-  DIRECTORY="homelab-setup"
-  ssh "$SERVER" "if [ ! -d '$DIRECTORY' ]; then mkdir $DIRECTORY; fi"
-  scp -r . "$SERVER:~/homelab-setup/." >/dev/null
-
-  figlet "Installing $tool"
-  ssh "$SERVER" "chmod +x ~/homelab-setup/${tool}.sh"
-  ssh -t "$SERVER" "bash ~/homelab-setup/${tool}.sh"
-}
-
 PRIVATE_KEY="empty"
 PUBLIC_KEY="empty"
+
 setup_sops() {
   if ! which sops >/dev/null 2>&1;
   then
@@ -61,23 +33,11 @@ setup_sops() {
 }
 
 
-if [[ "y" == $K8S ]]; then
-  echo "  "
-  read -p "Create Public and Private Key for SOPS (y/n): " SOPS
+echo "  "
+read -p "Create Public and Private Key for SOPS (y/n): " SOPS
 
-  if [[ "y" == $SOPS ]]; then
-    setup_sops
-  fi
-
-  install_tools "k8s"
-fi
-
-if [[ "y" == $PIHOLE ]]; then
-  install_tools "pihole"
-fi
-
-if [[ "y" == $NGINX ]]; then
-  install_tools "nginx-proxy"
+if [[ "y" == $SOPS ]]; then
+  setup_sops
 fi
 
 rm -rf age.agekey
